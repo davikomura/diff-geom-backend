150979f586c40b2ded774991edc7c943c7ca23e0
Davi Komura de Castro <davi.komura@gmail.com>
2025-07-04 16:33:01 +0100

    update permision cors

--- src/app/main.py
+++ src/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Differential Geometry Simulator")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://diff-geom-frontend.vercel.app/"],
+    allow_origins=["https://diff-geom-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
